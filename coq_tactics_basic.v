5257b8139b12e2489b19b6fdd883286f3ff543b5
ImPro <ImageProcessAlice@outlook.com>
2025-02-11 21:59:07 +0800

    complete assignment2

--- coq_tactics_basic.v
+++ coq_tactics_basic.v
@@ -69,3 +69,77 @@ Proof.
     apply H2.
     apply H3.
 Qed.
+
+Theorem example5: forall P Q: Prop,
+    (P \/ Q) -> (Q \/ P).
+Proof.
+    intros.
+    inversion H.
+    right.
+    apply H0.
+    left.
+    apply H0.
+Qed.
+
+Theorem exercise5: forall P Q R: Prop,
+    (P \/ (Q \/ R)) -> ((P \/ Q) \/ R).
+Proof.
+    intros.
+    inversion H.
+    left.
+    left.
+    apply H0.
+    inversion H0.
+    left.
+    right.
+    apply H1.
+    right.
+    apply H1.
+Qed.
+
+Theorem exercise6: forall P Q R: Prop,
+    ((P -> R) /\ (Q -> R)) -> (P /\ Q -> R).
+Proof.
+    intros.
+    inversion H.
+    inversion H0.
+    apply H1 in H3.
+    apply H3.
+Qed.
+
+Theorem exercise7: forall P Q R: Prop,
+        (P -> Q /\ R) -> ((P -> Q) /\ (P -> R)).
+Proof.
+    intros.
+    split.
+    apply H.
+    apply H.
+Qed.
+
+Theorem challenge1: forall P Q R:Prop,
+        (P /\ Q -> R) <-> (P -> Q -> R).
+Proof.
+  intros.
+  split.
+  intros.
+  apply H.
+  split.
+  apply H0.
+  apply H1.
+  intros.
+  apply H.
+  apply H0.
+  apply H0.
+Qed.
+
+Theorem challenge2: forall P Q R:Prop,
+        (P -> Q) -> (~Q -> ~P).
+Proof.
+  intros.
+  unfold not.
+  unfold not in H0.
+  intros.
+  apply H in H1.
+  apply H0 in H1.
+  apply H1.
+Qed.
